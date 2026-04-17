7064eb060728b7146fcd2acfba15ee620f84ab99
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-17 23:13:35 +0200

    Initialized trackwise api project

---
+++ src/main/java/tech/iraelie/trackwise_api/TrackwiseApiApplication.java
@@ -0,0 +1,13 @@
+package tech.iraelie.trackwise_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackwiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackwiseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/iraelie/trackwise_api/TrackwiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.iraelie.trackwise_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackwiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
