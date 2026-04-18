d958c8078224873f4e28dac2ce60076e873d6044
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-18 23:44:45 +0200

    created CategoryMapper for connecting Entities and DTOs

---
+++ src/main/java/tech/iraelie/trackwise_api/category/CategoryMapper.java
@@ -0,0 +1,27 @@
+package tech.iraelie.trackwise_api.category;
+
+import tech.iraelie.trackwise_api.model.Transaction;
+import tech.iraelie.trackwise_api.model.User;
+
+import java.util.List;
+
+public class CategoryMapper {
+    public static CategoryDTO toDto(Category category) {
+        if (category == null) return null;
+
+        return CategoryDTO.builder()
+                .name(category.getName())
+                .build();
+    }
+
+    public static Category toEntity(CategoryDTO dto, User user, List<Transaction> transactions) {
+        if (dto == null) return null;
+
+        Category category = new Category();
+        category.setName(dto.getName());
+        category.setUser(user);
+        category.setTransactions(transactions);
+
+        return category;
+    }
+}
