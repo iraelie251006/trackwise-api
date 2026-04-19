6243aef6e3308e5b3e8b65ca74c891241e3da12e
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-19 23:06:38 +0200

    Implemented User model entity

--- src/main/java/tech/iraelie/trackwise_api/model/User.java
+++ src/main/java/tech/iraelie/trackwise_api/model/User.java
@@ -1,7 +1,32 @@
 package tech.iraelie.trackwise_api.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import tech.iraelie.trackwise_api.category.Category;
 
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    private List<Transaction> transactions;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    private List<Category> categories;
 }
