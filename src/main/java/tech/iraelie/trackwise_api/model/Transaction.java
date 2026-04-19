68be622b33e114d02aefbb00795c6902c267424f
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-19 23:06:03 +0200

    Implemented Transaction model entity

--- src/main/java/tech/iraelie/trackwise_api/model/Transaction.java
+++ src/main/java/tech/iraelie/trackwise_api/model/Transaction.java
@@ -1,7 +1,33 @@
 package tech.iraelie.trackwise_api.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import tech.iraelie.trackwise_api.category.Category;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
 @Entity
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user")
+    private User user;
+
+    private LocalDate date;
 }
