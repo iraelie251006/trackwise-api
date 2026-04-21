64762b7a12be88805e8b1c0a9ce0b871cf3f8083
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-21 23:58:12 +0200

    inherited methods from basic CRUD

--- src/main/java/tech/iraelie/trackwise_api/user/UserRepository.java
+++ src/main/java/tech/iraelie/trackwise_api/user/UserRepository.java
@@ -3,4 +3,5 @@ package tech.iraelie.trackwise_api.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+//    this inherited methods from basic CRUD
 }
