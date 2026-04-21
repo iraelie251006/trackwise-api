9e6b5ab47c2807daa39c76a22410ecb3b212cb44
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-21 23:57:33 +0200

    removed JpaRepository from UserInterface

--- src/main/java/tech/iraelie/trackwise_api/user/UserInterface.java
+++ src/main/java/tech/iraelie/trackwise_api/user/UserInterface.java
@@ -1,6 +1,4 @@
 package tech.iraelie.trackwise_api.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInterface extends JpaRepository<User, String> {
+public interface UserInterface{
 }
