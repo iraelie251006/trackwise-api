fa9c53eff8e3d938507e79a2918b2493a1afe180
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-21 23:57:07 +0200

    UserRepository extended JpaRepository

--- src/main/java/tech/iraelie/trackwise_api/user/UserRepository.java
+++ src/main/java/tech/iraelie/trackwise_api/user/UserRepository.java
@@ -1,4 +1,6 @@
 package tech.iraelie.trackwise_api.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
 }
