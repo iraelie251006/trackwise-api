b5f780953bf283c742801a2fd984d0613f3754bd
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-22 23:51:19 +0200

    annotated @Repository to UserRepository interface

--- src/main/java/tech/iraelie/trackwise_api/user/UserRepository.java
+++ src/main/java/tech/iraelie/trackwise_api/user/UserRepository.java
@@ -1,7 +1,9 @@
 package tech.iraelie.trackwise_api.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 }
 
