00bd8fa7635c038f9f8cc3a6f854eae67da24402
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-22 23:50:28 +0200

    added @RequiredArgsConstructor to UserController class

--- src/main/java/tech/iraelie/trackwise_api/user/UserController.java
+++ src/main/java/tech/iraelie/trackwise_api/user/UserController.java
@@ -1,7 +1,9 @@
 package tech.iraelie.trackwise_api.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 }
