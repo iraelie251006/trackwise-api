b7fc3ecb549fe9886ea67f9acbad70c6eaf7572a
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-22 23:53:18 +0200

    annotated @RequiredArgsConstructor to UserService class

--- src/main/java/tech/iraelie/trackwise_api/user/UserService.java
+++ src/main/java/tech/iraelie/trackwise_api/user/UserService.java
@@ -1,7 +1,9 @@
 package tech.iraelie.trackwise_api.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 }
