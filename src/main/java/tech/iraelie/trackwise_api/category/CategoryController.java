10774e8e20def9be63e40af05848f9e8fdaeccdc
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-23 23:55:41 +0200

    added @RequiredArgsConstructor to CategoryController

--- src/main/java/tech/iraelie/trackwise_api/category/CategoryController.java
+++ src/main/java/tech/iraelie/trackwise_api/category/CategoryController.java
@@ -1,8 +1,10 @@
 package tech.iraelie.trackwise_api.category;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class CategoryController {
     private final CategoryInterface categoryService;
 }
