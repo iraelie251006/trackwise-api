ed9822e9ac884586f45e8d4ccbc72c5de6a47a98
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-23 23:54:34 +0200

    added @Setter to dto

--- src/main/java/tech/iraelie/trackwise_api/category/CategoryDTO.java
+++ src/main/java/tech/iraelie/trackwise_api/category/CategoryDTO.java
@@ -2,8 +2,10 @@ package tech.iraelie.trackwise_api.category;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
 public class CategoryDTO {
     private String name;
